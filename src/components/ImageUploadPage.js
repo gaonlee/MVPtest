4ec4b0398bc3a197a3807eff97d35ce462c04293
gaonlee <leemil1211@gmail.com>
2024-07-11 23:31:03 +0900

    빌드 경고 해결

--- src/components/ImageUploadPage.js
+++ src/components/ImageUploadPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import Header from './Header';
 import ImageUpload from './ImageUpload';

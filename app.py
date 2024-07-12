1d1cc58fd27da29bdaa356faeda2b3630c1c038c
gaonlee <leemil1211@gmail.com>
2024-07-12 16:28:43 +0900

    gunicorn 추가

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ logging.basicConfig(level=logging.DEBUG)
 load_dotenv()
 
 app = Flask(__name__, static_folder='./build', static_url_path='/')
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
 app.config['UPLOAD_FOLDER'] = 'uploads'
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)

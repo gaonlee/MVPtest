bbd022a791b5c24ed8de72356cb02642022cc698
gaonlee <leemil1211@gmail.com>
2024-07-15 12:07:07 +0900

    lambda_fuction.py 변경

--- api/lambda_function.py
+++ api/lambda_function.py
@@ -4,8 +4,13 @@ import requests
 
 def handler(request):
     api_url = os.getenv('REACT_APP_API_URL')
+    if not api_url:
+        return {
+            'statusCode': 500,
+            'body': json.dumps('API URL is not defined in environment variables.')
+        }
     try:
-        response = requests.get(api_url)
+        response = requests.get(api_url + '/data')
         data = response.json()
         return {
             'statusCode': 200,
